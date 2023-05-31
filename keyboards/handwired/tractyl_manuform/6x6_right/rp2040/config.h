9b9887650bc1c6b1eb5efef15ed0c8082d59a866
Admiral-Fish <admiralfish420@gmail.com>
2023-05-30 23:23:28 -0700

    6x6 tractly

---
+++ keyboards/handwired/tractyl_manuform/6x6_right/rp2040/config.h
@@ -0,0 +1,33 @@
+// Copyright 2023 Admiral-Fish (@Admiral-Fish)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define MASTER_RIGHT
+
+#define USE_SERIAL
+#define SERIAL_USART_FULL_DUPLEX 
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+#define SERIAL_PIO_USE_PIO0
+#define SERIAL_USART_TIMEOUT     100  // USART driver timeout. default 100
+#define SERIAL_USART_SPEED 921600
+#define SERIAL_USART_PIN_SWAP
+
+#define PMW33XX_CS_PIN GP21
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
